d3116ba46cf5c6d7be754b831c3e8a9aac7bf5c8
viniciusvieirac <vini.clau0@gmail.com>
2023-06-03 13:38:25 -0300

    feat: centraliza infos do perfil na sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,6 +7,9 @@ export function Sidebar() {
               src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
               alt="" />
             <div className={style.profile}>
+                <img src="https://avatars.githubusercontent.com/u/118015999?v=4"
+                 alt=""
+                 className={style.avatar} />
                 <strong>Vinicius Vieira</strong>
                 <span>Web Developer</span>
             </div>
